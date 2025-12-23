eefea869b18643e9ddab19045b3af3168f7f06f9
Jade Chi-yen <chiyenjade@gmail.com>
2025-12-23 16:10:19 +0100

    fix: infra error

--- infrastructure/src/adapters/db/mongo/factories/transaction.ts
+++ infrastructure/src/adapters/db/mongo/factories/transaction.ts
@@ -2,16 +2,19 @@ import { GetAllTransactionsByAccountUsecase } from "@application/usecases/transa
 import { MongoClient } from "../../MongoClient";
 import { UserRepositoryMongo } from "../repositories/UserRepositoryMongo";
 import { TransactionRepositoryMongo } from "../repositories/TransactionRepositoryMongo";
+import { AccountRepositoryMongo } from "../repositories/AccountRepositoryMongo";
 
 
 export const transactionFactory = () => {
     const client = new MongoClient();
     const userRepository = new UserRepositoryMongo(client);
     const transactionRepository = new TransactionRepositoryMongo(client);
+    const accountRepository = new AccountRepositoryMongo(client);
 
     const getAllByAccount = new GetAllTransactionsByAccountUsecase(
         userRepository,
-        transactionRepository
+        transactionRepository,
+        accountRepository
     )
 
     return {
