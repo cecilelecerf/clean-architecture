b7b55e755392e4368e8e5b4c8bffc875d586db1c
Jade Chi-yen <chiyenjade@gmail.com>
2026-01-07 19:30:07 +0100

    upd: credit routes order

--- interfaces/express/src/routes/credits.route.ts
+++ interfaces/express/src/routes/credits.route.ts
@@ -7,13 +7,14 @@ const creditsRouter: Router = express.Router();
 creditsRouter.get("/credits", authMiddleware, CreditsController.getAllByUser);
 creditsRouter.post("/credits", authMiddleware, CreditsController.request);
 
+creditsRouter.get("/credits/:creditId", authMiddleware, CreditsController.getCredit);
+creditsRouter.patch("/credits/:creditId", authMiddleware, CreditsController.applyMonthlyPaiement);
+
+creditsRouter.patch("/credits/:creditId/grant", authMiddleware, CreditsController.grantCredit);
+
 creditsRouter.get("/credits/users/:userId", authMiddleware, CreditsController.getOneByUser);
 
 creditsRouter.get("/credits/status", authMiddleware, CreditsController.getAllByStatus);
 
 creditsRouter.get("/credits/formules/:formuleId", authMiddleware, CreditsController.getAllByFormule);
 
-creditsRouter.get("/credits/:creditId", authMiddleware, CreditsController.getCredit);
-creditsRouter.patch("/credits/:creditId", authMiddleware, CreditsController.applyMonthlyPaiement);
-
-creditsRouter.patch("/credits/:creditId/grant", authMiddleware, CreditsController.grantCredit);
