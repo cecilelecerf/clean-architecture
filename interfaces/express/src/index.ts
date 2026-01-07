d347ad8639198363e8d22049a75c98d19275e7a4
cecilelecerf <c.lecerf.cecile@gmail.com>
2026-01-07 19:25:12 +0100

    fix and clean

--- interfaces/express/src/index.ts
+++ interfaces/express/src/index.ts
@@ -4,6 +4,8 @@ import cors from "cors";
 import threadRouter from "./routes/thread.router";
 import { authMiddleware } from "./middlewares/auth.middleware";
 import userRouter from "./routes/user.route";
+import savingsrateRouter from "./routes/savingsrate.router";
+import tagsRouter from "./routes/tags.router";
 
 const app = express();
 const port = 3002;
@@ -18,6 +20,8 @@ app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/threads", authMiddleware, threadRouter);
 app.use("/api/users", authMiddleware, userRouter);
+app.use("/api/savings-rate", authMiddleware, savingsrateRouter);
+app.use("/api/tags", authMiddleware, tagsRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
