fe2901d20f6f367b4212f2f3b1448dbac5a2f704
Jade Chi-yen <chiyenjade@gmail.com>
2026-01-11 20:46:18 +0100

    clean: user repo method

--- application/src/ports/repositories/UserRepository.ts
+++ application/src/ports/repositories/UserRepository.ts
@@ -6,10 +6,8 @@ export interface UserRepository {
   findById(id: UserEntity["id"]): Promise<UserEntity | null>;
   findByEmail(email: Email): Promise<UserEntity | null>;
   findByIban(iban: IBAN): Promise<UserEntity | null>;
-  findAll(): Promise<UserEntity[]>;
   findAllByRoleAndIsActif(role?: UserEntity["role"]): Promise<UserEntity[]>;
   save(user: UserEntity): Promise<void>;
   update(user: UserEntity): Promise<void>;
-  delete(id: UserEntity["id"]): Promise<void>;
   countUserByRole(role: UserEntity["role"]): Promise<number>;
 }
