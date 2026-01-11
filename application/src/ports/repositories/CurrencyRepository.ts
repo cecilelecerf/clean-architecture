4c33c179f028896833159acc85f816890354b0ee
Jade Chi-yen <chiyenjade@gmail.com>
2026-01-11 20:07:51 +0100

    clean: files

--- application/src/ports/repositories/CurrencyRepository.ts
+++ application/src/ports/repositories/CurrencyRepository.ts
@@ -2,11 +2,8 @@ import { CurrencyEntity } from "@domain/entities/CurrencyEntity";
 
 export interface CurrencyRepository {
   save(currency: CurrencyEntity): Promise<void>;
-
   update(currency: CurrencyEntity): Promise<void>;
-
   delete(code: string): Promise<void>;
-
   findByCode(code: string): Promise<CurrencyEntity | null>;
   findAll(): Promise<CurrencyEntity[]>;
 }
